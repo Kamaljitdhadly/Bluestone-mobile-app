e0ff5e4f035a6f796348ef9a96b392e7a6cdd1b0
Kamaljit Singh <kamaljitdhadly@gmail.com>
2022-02-22 10:07:23 +0530

    initial base structure of Project

---
+++ Bluestone/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Bluestone.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Bluestone.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
